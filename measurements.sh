4c3a3df3a14ae332fe75788984249a4785a33c7d
gystemd <gystemd@gmail.com>
2024-01-26 14:40:47 +0100

    modify measurements script

--- measurements.sh
+++ measurements.sh
@@ -1,33 +1,53 @@
-# echo "Running sequential\n"
-# ./HuffmanProject samples/1000000000.txt binary seq
 file_name=$1
-# check if file exists
 if [ ! -f "$file_name" ]
 then
     echo "File $file not found!"
     exit 1
 fi
-# check if the second argument is equal to y
-if [ "$2" = "y" ]
-then
-    echo "Running with jemalloc\n"
-    export LD_PRELOAD=/usr/local/lib/libjemalloc.so
-fi
+
 rm measurements/seq.csv
-echo "Running sequential\n"
+rm measurements/threads.csv
+rm measurements/ff.csv
+
+echo "Running sequential \n"
 ./HuffmanProject $file_name binary seq
 
-rm measurements/threads.csv
 for i in 2 4 8 16 32 64 128
 do
-    echo "Running with $i threads\n"
-    LD_PRELOAD=$LD_PRELOAD ./HuffmanProject $file_name binary t $i
+    echo "Running threads version with $i threads \n"
+     ./HuffmanProject $file_name binary t $i
 done
 
-rm measurements/ff.csv
 for i in 2 4 8 16 32 64 128
 do
-    echo "Running with $i threads\n"
-    LD_PRELOAD=$LD_PRELOAD ./HuffmanProject $file_name binary ff $i
+    echo "Running fastflow with $i threads \n"
+     ./HuffmanProject $file_name binary ff $i
 done
+mv measurements/seq.csv measurements/seq-nojemalloc.csv
+mv measurements/threads.csv measurements/threads-nojemalloc.csv
+mv measurements/ff.csv measurements/ff-nojemalloc.csv
+
+if [ "$2" = "jemalloc" ]
+then
+    export LD_PRELOAD=/usr/local/lib/libjemalloc.so
+
+    echo "Running sequential (jemalloc) \n"
+    ./HuffmanProject $file_name binary seq
+
+    for i in 2 4 8 16 32 64 128
+    do
+        echo "Running $i threads version (jemalloc) \n"
+        LD_PRELOAD=$LD_PRELOAD ./HuffmanProject $file_name binary t $i
+    done
+
+    for i in 2 4 8 16 32 64 128
+    do
+        echo "Running fastflow version ($i threads) (jemalloc) \n"
+        LD_PRELOAD=$LD_PRELOAD ./HuffmanProject $file_name binary ff $i
+    done
+fi
+
+mv measurements/seq.csv measurements/seq-jemalloc.csv
+mv measurements/threads.csv measurements/threads-jemalloc.csv
+mv measurements/ff.csv measurements/ff-jemalloc.csv
 
--- plot.py
+++ plot.py
@@ -3,37 +3,40 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 import numpy as np
 import pandas as pd
-sequential_data = pd.read_csv('measurements/seq.csv', sep=',', header=None)
-total_time_sequential = sequential_data.iloc[0, 2]
-threads_data = pd.read_csv('measurements/threads.csv', sep=',', header=None)
-ff_data = pd.read_csv('measurements/ff.csv', sep=',', header=None)
 
-plt.plot(threads_data.iloc[:, 0], threads_data.iloc[:, 2])
+seq_data = pd.read_csv('measurements/seq-nojemalloc.csv', sep=',', header=None)
+tot_time_seq = seq_data.iloc[0, 2]
+seq_data_jemalloc = pd.read_csv('measurements/seq-jemalloc.csv', sep=',', header=None)
+tot_time_seq_jemalloc = seq_data_jemalloc.iloc[0, 2]
+
+threads_data = pd.read_csv('measurements/threads-nojemalloc.csv', sep=',', header=None)
+threads_data_jemalloc = pd.read_csv('measurements/threads-jemalloc.csv', sep=',', header=None)
+
+ff_data = pd.read_csv('measurements/ff-nojemalloc.csv', sep=',', header=None)
+ff_data_jemalloc = pd.read_csv('measurements/ff-jemalloc.csv', sep=',', header=None)
+
 plt.gca().yaxis.set_major_formatter(mtick.FormatStrFormatter('%.0f'))
+plt.grid(True)
+
 plt.xlabel('number of workers')
 plt.ylabel('total time')
-plt.grid(True)
-plt.savefig('figures/threads.png')
+plt.plot(threads_data.iloc[:, 0], threads_data.iloc[:, 2], label='threads')
+plt.plot(threads_data_jemalloc.iloc[:, 0], threads_data_jemalloc.iloc[:, 2], label='threads jemalloc')
+plt.plot(ff_data.iloc[:, 0], ff_data.iloc[:, 2], label='ff')
+plt.plot(ff_data_jemalloc.iloc[:, 0], ff_data_jemalloc.iloc[:, 2], label='ff jemalloc')
+plt.legend()
+plt.savefig('figures/total-time.png')
 plt.clf()
 
-plt.plot(ff_data.iloc[:, 0], ff_data.iloc[:, 2])
-plt.gca().yaxis.set_major_formatter(mtick.FormatStrFormatter('%.0f'))
-plt.xlabel('number of workers')
-plt.ylabel('total time')
-plt.grid(True)
-plt.savefig('figures/ff.png')
-plt.clf()
-
-speedup_threads = total_time_sequential / threads_data.iloc[:, 2]
-plt.plot(threads_data.iloc[:, 0], speedup_threads)
 plt.xlabel('threads')
 plt.ylabel('speedup')
-plt.grid(True)
-plt.savefig('figures/speedup_threads.png')
-
-speedup_ff = total_time_sequential / ff_data.iloc[:, 2]
-plt.plot(ff_data.iloc[:, 0], speedup_ff)
-plt.xlabel('threads')
-plt.ylabel('speedup')
-plt.grid(True)
-plt.savefig('figures/speedup_ff.png')
+speedup_threads = tot_time_seq / threads_data.iloc[:, 2]
+plt.plot(threads_data.iloc[:, 0], speedup_threads, label='threads')
+speedup_threads_jemalloc = tot_time_seq_jemalloc / threads_data_jemalloc.iloc[:, 2]
+plt.plot(threads_data_jemalloc.iloc[:, 0], speedup_threads_jemalloc, label='threads jemalloc')
+speedup_ff = tot_time_seq / ff_data.iloc[:, 2]
+plt.plot(ff_data.iloc[:, 0], speedup_ff, label='ff')
+speedup_ff_jemalloc = tot_time_seq_jemalloc / ff_data_jemalloc.iloc[:, 2]
+plt.plot(ff_data_jemalloc.iloc[:, 0], speedup_ff_jemalloc, label='ff jemalloc')
+plt.legend()
+plt.savefig('figures/speedup.png')
