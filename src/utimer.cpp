66bc8301bc219837c03143339bf34ff76adb6da7
gystemd <gystemd@gmail.com>
2024-02-01 16:59:46 +0100

    formatting

--- src/utimer.cpp
+++ src/utimer.cpp
@@ -1,38 +1,40 @@
-#include <iostream>
-#include <iomanip>
 #include <chrono>
-
+#include <iomanip>
+#include <iostream>
 
 #define START(timename) auto timename = std::chrono::system_clock::now();
-#define STOP(timename,elapsed)  auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - timename).count();
-
+#define STOP(timename, elapsed)                                         \
+  auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>( \
+                     std::chrono::system_clock::now() - timename)       \
+                     .count();
 
 class utimer {
-    std::chrono::system_clock::time_point start;
-    std::chrono::system_clock::time_point stop;
-    std::string message;
-    using usecs = std::chrono::microseconds;
-    using msecs = std::chrono::milliseconds;
+  std::chrono::system_clock::time_point start;
+  std::chrono::system_clock::time_point stop;
+  std::string message;
+  using usecs = std::chrono::microseconds;
+  using msecs = std::chrono::milliseconds;
 
-private:
-    long * us_elapsed;
+ private:
+  long* us_elapsed;
 
-public:
+ public:
+  explicit utimer(std::string& m) : message(m), us_elapsed((long*)NULL) {
+    start = std::chrono::system_clock::now();
+  }
 
-    explicit utimer(std::string& m) : message(m), us_elapsed((long *)NULL) {
-        start = std::chrono::system_clock::now();
-    }
+  utimer(const std::string m, long* us) : message(m), us_elapsed(us) {
+    start = std::chrono::system_clock::now();
+  }
 
-    utimer(const std::string m, long * us) : message(m),us_elapsed(us) {
-        start = std::chrono::system_clock::now();
-    }
+  ~utimer() {
+    stop = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed = stop - start;
+    auto musec =
+        std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
 
-    ~utimer() {
-        stop = std::chrono::system_clock::now();
-        std::chrono::duration<double> elapsed = stop - start;
-        auto musec = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
-
-        // std::cout << message << " computed in " << std::setw(15) << musec << " usec " << std::endl;
-        if(us_elapsed != NULL)(*us_elapsed) = musec;
-    }
+    // std::cout << message << " computed in " << std::setw(15) << musec << "
+    // usec " << std::endl;
+    if (us_elapsed != NULL) (*us_elapsed) = musec;
+  }
 };
